d2f2309bf9592125605597e6e30efd28fc41ce51
ifeanyi23 <joseph.okorie@versent.com.au>
2024-04-01 13:27:14 +1000

    running tf plan

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -11,21 +11,21 @@ module "rds-db" {
 }
 
 
-# module "lambda" {
-#   source                 = "./modules/lambda"
-#   for_each               = var.lambda
-#   lambda                 = var.lambda[each.key]
-#   lambda_name            = each.key
-#   account_id             = local.workspace["account_id"]
-#   subnet_ids             = [aws_subnet.subnet_2.id, aws_subnet.subnet_3.id]
-#   vpc_security_group_ids = [aws_security_group.lambda_sg.id]
-#   # event_bridge_resource  = module.event_bridge[each.value.event_bridge_name].aws_cloudwatch_event_rule.lambda
-# }
+module "lambda" {
+  source                 = "./modules/lambda"
+  for_each               = var.lambda
+  lambda                 = var.lambda[each.key]
+  lambda_name            = each.key
+  account_id             = local.workspace["account_id"]
+  subnet_ids             = [aws_subnet.subnet_2.id, aws_subnet.subnet_3.id]
+  vpc_security_group_ids = [aws_security_group.lambda_sg.id]
+  # event_bridge_resource  = module.event_bridge[each.value.event_bridge_name].aws_cloudwatch_event_rule.lambda
+}
 
-# module "event_bridge" {
-#   source            = "./modules/event-bridge"
-#   for_each          = var.event_bridge
-#   event_bridge      = var.event_bridge[each.key]
-#   event_bridge_name = each.key
-#   lambda_arn        = module.lambda[each.value.function_name].lambda_arn
-# }
+module "event_bridge" {
+  source            = "./modules/event-bridge"
+  for_each          = var.event_bridge
+  event_bridge      = var.event_bridge[each.key]
+  event_bridge_name = each.key
+  lambda_arn        = module.lambda[each.value.function_name].lambda_arn
+}
